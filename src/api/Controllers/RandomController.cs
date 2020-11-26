bcf82a5d1f6b9c72a3f89d6bd8058fa6b5914014
martin@esmann.io <martin@esmann.io>
2020-11-26 08:27:07 +0100

    initial

---
+++ src/api/Controllers/RandomController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomController : ControllerBase
+    {
+        private readonly ILogger<RandomController> _logger;
+
+        public RandomController(ILogger<RandomController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("called RandomController");
+            return Guid.NewGuid().ToString("D");
+        }
+    }
+}
