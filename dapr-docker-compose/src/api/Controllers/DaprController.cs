09e00162fbb1da796be7cc46a8892ccc32d4c303
martin@esmann.io <martin@esmann.io>
2020-11-27 15:57:07 +0100

    dapr with docker-compose wp

---
+++ dapr-docker-compose/src/api/Controllers/DaprController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudNative.CloudEvents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DaprController : ControllerBase
+    {
+        private readonly ILogger<DaprController> _logger;
+
+        public DaprController(ILogger<DaprController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("subscribe")]
+        public ActionResult Subscribe()
+        {
+            var payload = new[]
+            {
+                new {pubsubname="messagebus", topic= "notification", route = "notification" }
+            };
+            return Ok(payload);
+        }
+
+        [HttpPost("/notification")]
+        public ActionResult Notification(CloudEvent cloudEvent)
+        {
+            _logger.LogDebug($"Cloud event {cloudEvent.Id} {cloudEvent.Type} {cloudEvent.DataContentType}");
+            _logger.LogInformation("Notification received...");
+            return Ok();
+        }
+    }
+}
--- dapr-docker-compose/src/web/Pages/Index.cshtml.cs
+++ dapr-docker-compose/src/web/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,17 +14,26 @@ namespace web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _clientFactory;
 
         public string Message { get; private set; } = "PageModel in C#";
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
         public async Task OnGet()
         {
             _logger.LogInformation("Index called");
+            await GetApiData();
+            await SendNotificationToApi();
+        }
+
+        private async Task GetApiData()
+        {
+            _logger.LogInformation("GetApiData called");
 
             try
             {
@@ -38,6 +49,31 @@ namespace web.Pages
             {
                 Message = ex.Message;
                 _logger.LogError(ex, "get api data error");
+            }
+        }
+
+        private async Task SendNotificationToApi()
+        {
+            _logger.LogInformation("SendNotificationToApi called");
+            string PUBSUB_NAME = "messagebus";
+
+            try
+            {
+                HttpClient client = _clientFactory.CreateClient("dapr");
+
+                var payload = JsonSerializer.Serialize(new { content = "some random value (" + Guid.NewGuid().ToString("D") + ")" });
+                var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync($"v1.0/publish/{PUBSUB_NAME}/notification", content);
+                _logger.LogInformation($"Notification send ({response.IsSuccessStatusCode}) - ({payload})");
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"Notification error ({response.IsSuccessStatusCode}) - ({await response.Content.ReadAsStringAsync()})");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Message = ex.Message;
+                _logger.LogError(ex, "Notification send error");
             }
         }
     }
