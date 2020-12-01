365c59646498b39399e35aa396ac4d071361376c
martin@esmann.io <martin@esmann.io>
2020-12-01 10:13:19 +0100

    update to dapr sample

---
+++ dapr-docker-compose/src/web/Pages/Messages.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using web.Model;
+
+namespace web.Pages
+{
+    public class MessageModel : PageModel
+    {
+        private readonly ILogger<MessageModel> _logger;
+        private readonly HttpClient _apiClient;
+
+        public IEnumerable<Message> Messages { get; private set; }
+
+        public MessageModel(ILogger<MessageModel> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _apiClient = clientFactory.CreateClient("api");
+        }
+
+        public async Task OnGet()
+        {
+            _logger.LogInformation("Messages called");
+
+            var json = await _apiClient.GetStringAsync("/data");
+            Messages = JsonConvert.DeserializeObject<List<Message>>(json);
+        }
+    }
+}
